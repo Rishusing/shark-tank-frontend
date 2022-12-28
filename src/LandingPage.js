21c2eed0acbc7f83bd3dedadd690aea030fc608c
Rishu Singh <rishu.singh@futuretic.co.in>
2022-12-28 11:08:29 +0530

    after change in landing page

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,31 +1,91 @@
 import React from 'react'
 import LandingNav from './partials/Header/LandingNav'
-import SimpleImageSlider from "react-simple-image-slider";
 import Footer from './partials/Footer/Footer';
+import './LandingPage.css'
+import {FaArrowCircleRight} from 'react-icons/fa'
 
-
-const images = [
-    { url: 'https://firebasestorage.googleapis.com/v0/b/preboardresult.appspot.com/o/1.png?alt=media&token=10e3fcf7-3e2f-4290-829d-119d5d825fa2' },
-    { url: 'https://firebasestorage.googleapis.com/v0/b/preboardresult.appspot.com/o/2.png?alt=media&token=7d29d1c8-c4b1-4044-8a52-69b707145211' },
-    { url: 'https://firebasestorage.googleapis.com/v0/b/preboardresult.appspot.com/o/3.png?alt=media&token=0285115d-4bb2-49cd-aefd-adbb9e10ed25' },
-
-];
 
 const LandingPage = () => {
 
     return (
         <div className='landing-page-main'>
             <LandingNav />
-            <div className="slider-container">
-                <SimpleImageSlider
-                    width={1000}
-                    height={450}
-                    images={images}
-                    showBullets={true}
-                    showNavs={true}
-                />
+            <div className='landingpage_main'>
+                <div className="landingpage_left">
+                    <div className='landingpage_left_top'>
+                        <div className="left1 left_top">
+                            <p>WHAT TO SCALE UP</p>
+                        </div>
+                        <div className="left2 left_top">
+                            <p>YOUR BUSINESS</p>
+                        </div>
+                        <div className="left3 left_top">
+                            <p>BUT NO ONE BELIEVES</p>
+                        </div>
+                        <div className="left4 left_top">
+                            IN YOU VISION?
+                        </div>
+                    </div>
+                    <div className='landingpage_left_bottom'>
+                        <div className="left_bottom">
+                            <FaArrowCircleRight />
+                            <p>NOW, PITCH IT TO RIGHT PEOPLE!</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="landingpage_middle">
+                    <div className="middle1 middlechild">
+                        <p>BE WHERE</p>
+                    </div>
+                    <div className="middle2 middlechild">
+                        <p>YOU BELONG</p>
+                    </div>
+                    <div className="middle3 middlechild">
+                        <p>BE WHERE</p>
+                    </div>
+                    <div className="middle4 middlechild">
+                        <p>YOUR IDEAS</p>
+                    </div>
+                    <div className="middle5 middlechild">
+                        <p>GET WINGS</p>
+                    </div>
+                </div>
+                <div className="landingpage_right">
+                    <div className="right1 right">
+                        <div className="right1_left">
+                            <p>Register/SignUp for Investor/Entrepreneur</p>
+                        </div>
+                        <div className="right1_right">
+                            <p>STEP1</p>
+                        </div>
+                    </div>
+                    <div className="right2 right">
+                        <div className="right2_left">
+                            <p>STEP2</p>
+                        </div>
+                        <div className="right2_right">
+                            <p>Create your profile/Enter you personal details</p>
+                        </div>
+                    </div>
+                    <div className="right3 right">
+                        <div className="right1_left">
+                            <p>Tell us about your business idea venture in detail</p>
+                        </div>
+                        <div className="right1_right">
+                            <p>STEP3</p>
+                        </div>
+                    </div>
+                    <div className="right4 right">
+                        <div className="right2_left">
+                            <p>STEP4</p>
+                        </div>
+                        <div className="right2_right">
+                            <p>Share your expectations in terms of investment</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <Footer/>
+            <Footer />
         </div>
     )
 }
